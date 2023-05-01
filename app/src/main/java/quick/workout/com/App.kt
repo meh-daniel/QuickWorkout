8f2b2730347fc716fcf43101f1daf0e435d1afcf
daniel <meh.daniel@yandex.com>
2023-05-01 14:08:57 +0700

    App/MainActivity

---
+++ app/src/main/java/quick/workout/com/App.kt
@@ -0,0 +1,5 @@
+package quick.workout.com
+
+import android.app.Application
+
+class App: Application()
---
+++ app/src/main/java/quick/workout/com/presentation/MainActivity.kt
@@ -0,0 +1,7 @@
+package quick.workout.com.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import quick.workout.com.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
